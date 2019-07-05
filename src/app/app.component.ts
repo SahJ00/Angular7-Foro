b740bd36b45ea590e620c084002b4a410f39b64f
SahJ00 <salvador.henares@gmail.com>
2019-07-05 14:16:23 +0200

    Actualizacion 05071019 1415 Terminado el user edit y añadiendo nuevo modulo de panel de control

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UserService } from './services/user.service';
+import { global } from './services/global';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
+})
+export class AppComponent implements OnInit, DoCheck {
+  public title = 'foro-angular';
+  public identity;
+  public token;
+  public url;
+
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
+
+  ) {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.url = global.url;
+  }
+
+  ngOnInit() {
+    console.log(this.identity)
+    console.log(this.token)
+  }
+
+  ngDoCheck() {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
+
+  logout() {
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
+    this._router.navigate(['/inicio']);
+  }
+}
